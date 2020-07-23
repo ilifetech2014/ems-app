6a591abbd0a7dc187ed8f61ecc476e827f004e52
ilifetech2014 <ilifetech2014@.github.com>
2020-07-23 14:32:56 +0530

    Updates

---
+++ precache-manifest.177bdb6e913e2ff507219da04e595fc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5a033fa422f2ffd70d3f43c0d149fe",
+    "url": "/ems-app/index.html"
+  },
+  {
+    "revision": "2cc847a35d0ae491912a",
+    "url": "/ems-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e26abd94a81d331557c1",
+    "url": "/ems-app/static/js/2.afcf42f9.chunk.js"
+  },
+  {
+    "revision": "2cc847a35d0ae491912a",
+    "url": "/ems-app/static/js/main.7357d8ee.chunk.js"
+  },
+  {
+    "revision": "2350b7f77f872725cbfa",
+    "url": "/ems-app/static/js/runtime~main.32973241.js"
+  }
+]);
